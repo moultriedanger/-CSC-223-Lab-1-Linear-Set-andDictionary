fa6d146d5cd0b77f5cc843b72d1b4d930b9675bf
Moultrie Dangerfield <moultriedanger@gmail.com>
2023-08-24 14:59:23 -0400

    Changes Added

---
+++ src/utilities/ArraySet.java
@@ -0,0 +1,21 @@
+package utilities;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+import java.util.Spliterator;
+
+public class ArraySet<E> implements List<E>, Set<E>
+{
+	protected ArrayList<E> _list;
+
+	public ArraySet()
+	{
+		_list = new ArrayList<E>();
+	}
+
+
+}
